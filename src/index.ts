eea21620830ada1a7ac8b0a8bfced83a3316618f
ayan4m1 <andrew@bulletlogic.com>
2024-03-06 00:42:56 -0500

    tweak description text for arg/option

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ program
   .description('Performs a "sparse" (i.e. partial) clone of a Git repository.')
   .argument('<working-copy>', 'Local Git working copy location')
   .argument('<repo-url>', 'Git repository clone URL')
-  .argument('<paths...>', 'One or more paths to check out')
-  .option('-f, --force', 'Forces overwriting existing working copy')
+  .argument('<paths...>', 'One or more paths to include in checkout')
+  .option('-f, --force', 'Force overwriting existing working copy')
   .option('-g, --globs', 'Allow use of glob patterns in <paths...> argument')
   .action(cloneSparse)
   .parse();
