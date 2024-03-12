70adcbdac7bf65e19cf658c4cbb5a602c2b799c5
ayan4m1 <andrew@bulletlogic.com>
2024-03-11 20:39:57 -0400

    refactor arguments to commander from inline to const style

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,18 @@ import { program } from 'commander';
 import { cloneSparse, getPackageVersion } from './utils.js';
 
 try {
+  const version = await getPackageVersion();
+  const handler = (cwd: string, repoUrl: string, paths: string[]) => {
+    const { globs, force } = program.opts();
+
+    cloneSparse(cwd, repoUrl, paths, {
+      force,
+      globs
+    });
+  };
+
   await program
-    .version(await getPackageVersion())
+    .version(version)
     .description(
       'Performs a "sparse" (i.e. partial) clone of a Git repository.'
     )
@@ -13,17 +23,11 @@ try {
     .argument('<paths...>', 'One or more paths to include in checkout')
     .option('-f, --force', 'Force overwriting existing working copy')
     .option('-g, --globs', 'Allow use of glob patterns in <paths...> argument')
-    .action((cwd: string, repoUrl: string, paths: string[]) => {
-      const { globs, force } = program.opts();
-
-      cloneSparse(cwd, repoUrl, paths, {
-        force,
-        globs
-      });
-    })
+    .action(handler)
     .parseAsync();
 } catch (error) {
   console.error(error);
+  process.exit(1);
 }
 
 export default cloneSparse;
