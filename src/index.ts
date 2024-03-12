28cdad0f06d6d73ba1e4c4b9552f9b7cdfbe7a10
ayan4m1 <andrew@bulletlogic.com>
2024-03-11 20:42:41 -0400

    continue refactoring arguments

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { program } from 'commander';
 import { cloneSparse, getPackageVersion } from './utils.js';
 
 try {
+  const description =
+    'Performs a "sparse" (i.e. partial) clone of a Git repository.';
   const version = await getPackageVersion();
   const handler = (cwd: string, repoUrl: string, paths: string[]) => {
     const { globs, force } = program.opts();
@@ -15,9 +17,7 @@ try {
 
   await program
     .version(version)
-    .description(
-      'Performs a "sparse" (i.e. partial) clone of a Git repository.'
-    )
+    .description(description)
     .argument('<working-copy>', 'Local Git working copy location')
     .argument('<repo-url>', 'Git repository clone URL')
     .argument('<paths...>', 'One or more paths to include in checkout')
