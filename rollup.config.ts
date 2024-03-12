8f6ee692d2cf0dc1f6a307b2957a3a49db326f25
ayan4m1 <andrew@bulletlogic.com>
2024-03-11 20:24:07 -0400

    add shebang to the start of lib/index.js

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,17 @@
 import terser from '@rollup/plugin-terser';
+import shebang from 'rollup-plugin-shebang-bin';
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import autoExternal from 'rollup-plugin-auto-external';
 
-const plugins = [autoExternal(), typescript(), nodeResolve()];
+const plugins = [
+  autoExternal(),
+  typescript(),
+  nodeResolve(),
+  shebang({
+    include: ['**/*.ts']
+  })
+];
 
 if (process.env.NODE_ENV === 'production') {
   plugins.push(terser());
@@ -12,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
 export default {
   input: './src/index.ts',
   output: {
-    file: './lib/index.js',
+    dir: './lib',
     format: 'esm'
   },
   plugins
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,28 @@ import { program } from 'commander';
 
 import { cloneSparse, getPackageVersion } from './utils.js';
 
-program
-  .version(await getPackageVersion())
-  .description('Performs a "sparse" (i.e. partial) clone of a Git repository.')
-  .argument('<working-copy>', 'Local Git working copy location')
-  .argument('<repo-url>', 'Git repository clone URL')
-  .argument('<paths...>', 'One or more paths to include in checkout')
-  .option('-f, --force', 'Force overwriting existing working copy')
-  .option('-g, --globs', 'Allow use of glob patterns in <paths...> argument')
-  .action((cwd: string, repoUrl: string, paths: string[]) => {
-    const { globs, force } = program.opts();
+try {
+  await program
+    .version(await getPackageVersion())
+    .description(
+      'Performs a "sparse" (i.e. partial) clone of a Git repository.'
+    )
+    .argument('<working-copy>', 'Local Git working copy location')
+    .argument('<repo-url>', 'Git repository clone URL')
+    .argument('<paths...>', 'One or more paths to include in checkout')
+    .option('-f, --force', 'Force overwriting existing working copy')
+    .option('-g, --globs', 'Allow use of glob patterns in <paths...> argument')
+    .action((cwd: string, repoUrl: string, paths: string[]) => {
+      const { globs, force } = program.opts();
 
-    cloneSparse(cwd, repoUrl, paths, {
-      force,
-      globs
-    });
-  })
-  .parse();
+      cloneSparse(cwd, repoUrl, paths, {
+        force,
+        globs
+      });
+    })
+    .parseAsync();
+} catch (error) {
+  console.error(error);
+}
 
 export default cloneSparse;
