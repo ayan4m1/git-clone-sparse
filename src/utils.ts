a64044aabfcccf5ec6b19a70e7aca47f00e81f8b
ayan4m1 <andrew@bulletlogic.com>
2025-08-25 18:12:10 -0400

    feat!: support git 2.37.1+

--- src/utils.ts
+++ src/utils.ts
@@ -21,7 +21,7 @@ const getInstallDirectory = (): string =>
 const getPackageJsonPath = (): string =>
   resolve(getInstallDirectory(), '..', 'package.json');
 
-const isGitInstallValid = async (): Promise<boolean> => {
+const gitVersionSatisfies = async (semver: string): Promise<boolean> => {
   try {
     const [version] = await executeCommand("git -v | cut -d' ' -f3");
 
@@ -30,11 +30,14 @@ const isGitInstallValid = async (): Promise<boolean> => {
       return false;
     }
 
-    return satisfies(gitVersion, '>=2.25.0');
+    return satisfies(gitVersion, semver);
   } catch {
     return false;
   }
 };
+
+const isGitVersionMinimum = async () => gitVersionSatisfies('>=2.25.0');
+const useNewCommandSyntax = async () => gitVersionSatisfies('>=2.37.1');
 
 const executeCommand = async (
   command: string,
@@ -53,11 +56,14 @@ const executeGitCommand = (cwd: string, args: string[] = []): Promise<void> =>
   new Promise(
     (resolve: () => void, reject: (reason?: string | Error) => void) => {
       const stderr = [];
-      const process = spawn('git', args, { cwd, stdio: [0, 0, 'pipe'] });
+      const process = spawn('git', args, { cwd });
 
       process.stderr.setEncoding('utf-8');
       process.stderr.on('data', (chunk) => stderr.push(chunk));
-      process.on('error', () => reject(`Error occurred during execution!`));
+      process.on('error', (error) => {
+        console.error(error);
+        reject(`Error occurred during execution!`);
+      });
       process.on('close', (code: number) =>
         code === 0
           ? resolve()
@@ -80,7 +86,7 @@ export async function cloneSparse(
       throw new Error('Invalid arguments supplied!');
     }
 
-    if (!isGitInstallValid()) {
+    if (!(await isGitVersionMinimum())) {
       throw new Error('Git >= 2.25.0 was not found!');
     }
 
@@ -110,32 +116,57 @@ export async function cloneSparse(
       throw new Error(`${cwd} already exists, refusing to overwrite it!`);
     }
 
+    const newSyntax = await useNewCommandSyntax();
+
     console.log('Pre-flight checks passed, performing bare clone...');
-    await executeGitCommand(workingCopyParent, [
-      'clone',
-      '-n',
-      '--depth',
-      '1',
-      '--filter=tree:0',
-      repoUrl,
-      workingCopyDir
-    ]);
-    console.log('Adding desired paths to sparse-checkout...');
-    paths = paths.map((path) => path.replace(/"/g, '').replace(/^\./, ''));
-    if (!globs) {
-      await executeGitCommand(cwd, [
-        'sparse-checkout',
-        'set',
-        '--no-cone',
-        ...paths
+
+    if (newSyntax) {
+      await executeGitCommand(workingCopyParent, [
+        'clone',
+        '--filter=blob:none',
+        '--no-checkout',
+        '--depth',
+        '1',
+        '--sparse',
+        repoUrl,
+        workingCopyDir
       ]);
     } else {
-      await executeGitCommand(cwd, ['sparse-checkout', 'init']);
-      await executeCommand(`echo !/* > .git/info/sparse-checkout`, cwd);
-      for (const path of paths) {
-        await executeCommand(`echo ${path} >> .git/info/sparse-checkout`, cwd);
+      await executeGitCommand(workingCopyParent, [
+        'clone',
+        '-n',
+        '--depth',
+        '1',
+        '--filter=tree:0',
+        repoUrl,
+        workingCopyDir
+      ]);
+    }
+    console.log('Adding desired paths to sparse-checkout...');
+    paths = paths.map((path) => path.replace(/"/g, '').replace(/^\./, ''));
+
+    if (newSyntax) {
+      await executeGitCommand(cwd, ['sparse-checkout', 'add', ...paths]);
+    } else {
+      if (!globs) {
+        await executeGitCommand(cwd, [
+          'sparse-checkout',
+          'set',
+          '--no-cone',
+          ...paths
+        ]);
+      } else {
+        await executeGitCommand(cwd, ['sparse-checkout', 'init']);
+        await executeCommand(`echo !/* > .git/info/sparse-checkout`, cwd);
+        for (const path of paths) {
+          await executeCommand(
+            `echo ${path} >> .git/info/sparse-checkout`,
+            cwd
+          );
+        }
       }
     }
+
     console.log('Performing final checkout...');
     // todo: add progress bar
     await executeGitCommand(cwd, ['checkout']);
